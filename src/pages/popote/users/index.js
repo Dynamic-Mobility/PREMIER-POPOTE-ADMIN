f0d3eaee1778e21edae5168f29f651abd5bb5efa
MarcosDMT <marcosgav80@gmail.com>
2023-08-24 13:14:55 +0300

    added users table

--- src/pages/popote/users/index.js
+++ src/pages/popote/users/index.js
@@ -2,13 +2,18 @@ import React from "react";
 import { DashboardLayout } from "../../../components/layouts/dashboard";
 import { AuthGuard } from "../../../hocs/auth-guard";
 import { Typography, Card, Container } from "@mui/material";
+import UsersDataGrid from "../../../components/popote/users/users-data-grid";
+import MKTypography from "../../../components/@mui-components/typography";
+
+
 
 const Users = () => {
   return (
     <>
-      <Container maxWidth="xl">
+      <Container maxWidth="xl" sx={{py:2}}>
+        <MKTypography sx={{my:1}} fontWeight={"semibold"} fontSize={"20px"}>User Management</MKTypography>
         <Card sx={{ p: 3 }}>
-          <Typography>Create Users</Typography>
+          <UsersDataGrid />
         </Card>
       </Container>
     </>
@@ -19,8 +24,7 @@ Users.getLayout = (page) => {
   return (
     <>
       {/* <AuthGuard> */}
-        <DashboardLayout>{page}</DashboardLayout>; //{" "}
-      {/* </AuthGuard> */}
+      <DashboardLayout>{page}</DashboardLayout>; // {/* </AuthGuard> */}
     </>
   );
 };
