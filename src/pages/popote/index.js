762e2884d9687e1c3616a28d40bb0b7077681c1e
MarcosDMT <marcosgav80@gmail.com>
2023-08-25 17:03:37 +0300

    mocked the dashboard cards

--- src/pages/popote/index.js
+++ src/pages/popote/index.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { DashboardLayout } from "../../components/layouts/dashboard";
+import MKTypography from "../../components/@mui-components/typography";
+import { Container, Grid, Card } from "@mui/material";
+import MKBox from "../../components/@mui-components/box";
 
-const index = () => {
+const Dashboard = () => {
   return (
-    <div>index</div>
-  )
-}
+    <>
+      <Container maxWidth="xl" sx={{ py: 2 }}>
+        <MKTypography fontWeight="bold" fontSize="20px">
+          Dashboard
+        </MKTypography>
+        <Grid container spacing={2}>
+          <Grid item md={3} xs={12}>
+            <Card sx={{p:2}}>Dashboard</Card>
+          </Grid>
+          <Grid item md={3} xs={12}>
+            <Card sx={{p:2}}>Dashboard</Card>
+          </Grid>
+          <Grid item md={3} xs={12}>
+            <Card sx={{p:2}}>Dashboard</Card>
+          </Grid>
+          <Grid item md={3} xs={12}>
+            <Card sx={{p:2}}>Dashboard</Card>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} sx={{my:1}}>
+          <Grid item md={3} xs={12}>
+            <Card sx={{p:2}}>Dashboard</Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+};
 
-export default index
+Dashboard.getLayout = (pages) => <DashboardLayout>{pages}</DashboardLayout>;
+
+export default Dashboard;
